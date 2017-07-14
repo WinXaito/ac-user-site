ca0f1b6a51a2cc62929ca0863adeca9261b24f86
WinXaito <github@winlap.ch>
2017-07-14 17:21:42 +0200

    Mise à jour du Schema SQL (Ajout du IF NOT EXISTS) pour éviter des problèmes

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name varchar,
 	password varchar,
 	grade integer
 );
 
-CREATE TABLE files (
+CREATE TABLE IF NOT EXISTS files (
 	id integer PRIMARY KEY AUTOINCREMENT,
 	name varchar,
 	user integer,
